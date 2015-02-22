2ce02e70cb2e7ee8effba168e589fe44d9fbc9f4
chris <chris@indefini.org>
2015-02-23 01:04:10 +0900

    rust update

--- build.rs
+++ build.rs
@@ -2,17 +2,18 @@ extern crate gcc;
 
 use std::default::Default;
 use std::os;
+use std::ffi::AsOsStr;
 
 fn main() {
-    let mut cfg: gcc::Config = Default::default();
+    let mut cfg = gcc::Config::new();
 
-    let src_dir = Path::new(os::getenv("CARGO_MANIFEST_DIR").unwrap()).join("png-sys/libpng-1.6.16");
-    cfg.include_directories.push(src_dir);
+    let mut src_dir = os::getenv("CARGO_MANIFEST_DIR").unwrap();
+    src_dir.push_str("png-sys/libpng-1.6.16");
+    cfg.include(src_dir.as_os_str());
 
-    let dep_dir = Path::new(os::getenv("DEP_PNG_ROOT").unwrap());
-    cfg.include_directories.push(dep_dir);
+    let dep_dir = os::getenv("DEP_PNG_ROOT").unwrap();
+    cfg.include(dep_dir.as_os_str());
 
     gcc::compile_library("libpngshim.a",
-                         &cfg,
                          &["src/shim.c"]);
 }
